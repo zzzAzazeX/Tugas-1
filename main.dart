efb94af41d958c65aa59f3396446d13d022eadc2
zzzAzazeX <balthazarblake12345@gmail.com>
2023-09-11 14:13:30 +0700

    tugas

--- main.dart
+++ main.dart
@@ -2,14 +2,18 @@ class Mahasiswa {
   String nim;
   String nama;
   int tahunLahir;
+  var usia;
 
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    usia = DateTime.now().year - tahunLahir;
+    print(
+        "Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021056", "Muhammad Hanif", 2002);
+  mhs.perkenalan();
 }
